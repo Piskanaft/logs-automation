221c9b22f32d7316bbe922c80d658a05d632e7b8
Piskanaft <cambogiyt@gmail.com>
2023-02-17 18:47:20 +0500

    2 cell gap, styling, filename choosing, newbuttons

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import os
 from math import pi
 from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
+from openpyxl.utils import get_column_letter
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
@@ -16,31 +18,67 @@ class MainWindow(base_class):
         self.connect_buttons()
         
     def connect_buttons(self):
-        self.ui.select_file_btn.clicked.connect(self.load_catalog)
-        self.ui.write_btn.clicked.connect(self.write_button_pressed)
+        self.ui.select_txt_btn.clicked.connect(lambda: self.load_file('Txt','txt'))
+        self.ui.select_existing_excel_btn.clicked.connect(lambda: self.load_file('Excel','xlsx'))
+        self.ui.write_new_btn.clicked.connect(self.write_new_button_pressed)
+        # self.ui.write_existing_btn.clicked.connect()
+    # def load_txt(self):
+    #     file_path = qtw.QFileDialog.getOpenFileName(self, 'Open File',filter="Txt (*.txt)")
+    #     if not file_path:
+    #         return None
+    #     self.file_path = file_path[0]
+        
+    #     selected_file_name = os.path.basename(self.file_path)
+    #     self.ui.selected_txt_lbl.setText(f'Выбран файл\n{selected_file_name}')
+    # def load_excel(self):
+    #     file_path = qtw.QFileDialog.getOpenFileName(self, 'Open File',filter="Excel (*.xlsx)")
 
-    def load_catalog(self):
-        file_path = qtw.QFileDialog.getOpenFileName(self, 'Open File',filter="Txt (*.txt)")
+
+    def load_file(self, format, extenion):
+
+        file_path = qtw.QFileDialog.getOpenFileName(self, 'Open File',filter=f"{format} (*.{extenion})")
         if not file_path:
             return None
-        self.file_path = file_path[0]
         
-        selected_file_name = os.path.basename(self.file_path)
-        self.ui.selected_file_lbl.setText(f'Выбран файл\n{selected_file_name}')
-    def write_button_pressed(self):
+        selected_file_name = os.path.basename(file_path[0])
+        if format == 'Excel':
+            print('here')
+            print(selected_file_name)
+            self.ui.selected_xlsx_lbl.setText(f'Выбран файл\n{selected_file_name}')
+            self.excel_file_path = file_path[0]
+        else:
+            self.ui.selected_txt_lbl.setText(f'Выбран файл\n{selected_file_name}')
+            self.txt_file_path = file_path[0]
+    def write_new_button_pressed(self):
         logs = self.compose_logs()
-        print(logs)
+        
         wb = Workbook()
         ws = wb.active
-        for row in logs:
-            ws.append(row)
-        wb.save('doc.xlsx')
+        alignment = Alignment(horizontal='center')
+        for row in ws.iter_rows(min_row = 1, max_row = len(logs), min_col = 3, max_col = len(logs[0])+2):
+            for cell in row:
+                cell.value = logs[cell.row-1][cell.column-3]
+                cell.alignment = alignment
+        self.set_styles(ws,len(logs[0]))
+        wb.save(f'{self.ui.file_name_lineEdit.text()}.xlsx')
+    def set_styles(self,ws, length):
+        bold = Font(color="00000000", bold  = True)
+        
+        for row in ws.iter_rows(min_row = 1, max_row = 1, min_col=3, max_col = 2+length):
+            for cell in row:
+                cell.font = bold
+                ws.column_dimensions[get_column_letter(cell.column)].width = 12
+                
+        
+        
+
     def compose_logs(self):
         logs = [['Date', 'Origin time', 'Lat', 'Lon', 'Depth', 'DepthMIN','DepthMAX', 'M', 'AzMajor', 'Rminor', 'Rmajor', 'S', 'N stations']]
-        with open(self.file_path,'r',encoding='utf-8') as file:
+        with open(self.txt_file_path,'r',encoding='utf-8') as file:
             text = file.read()
             events = text.split('END EVENT')
             events.pop()
+
             for event in events:
                 event_log = []
                 event = event.strip()
@@ -60,7 +98,8 @@ class MainWindow(base_class):
                 AzMajor, Rminor, Rmajor = [d.split('=')[1] for d in azimuth.split()]
                 S = float(Rminor) * float(Rmajor) * pi
                 Nstat = event[event.find('NSTAT'):event.find('NPHASES')-1].split('=')[1]
-                event_log = [date,time.replace('.',':'),round(lat,3),round(lon,3), depth, depthmin, depthmax, M, AzMajor, Rminor, Rmajor, S, Nstat]
+                event_log = [date,time.replace('.',':'),round(lat,3),round(lon,3), int(depth), int(depthmin), int(depthmax), M, float(AzMajor), float(Rminor), float(Rmajor), round(S,2), int(Nstat)]
+                
                 logs.append(event_log)
         return logs
 
---
+++ ui_gui.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'gui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QPushButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.select_txt_btn = QPushButton(self.centralwidget)
+        self.select_txt_btn.setObjectName(u"select_txt_btn")
+        self.select_txt_btn.setGeometry(QRect(90, 110, 141, 41))
+        self.selected_txt_lbl = QLabel(self.centralwidget)
+        self.selected_txt_lbl.setObjectName(u"selected_txt_lbl")
+        self.selected_txt_lbl.setGeometry(QRect(80, 210, 361, 31))
+        self.write_new_btn = QPushButton(self.centralwidget)
+        self.write_new_btn.setObjectName(u"write_new_btn")
+        self.write_new_btn.setGeometry(QRect(400, 110, 131, 41))
+        self.file_name_lineEdit = QLineEdit(self.centralwidget)
+        self.file_name_lineEdit.setObjectName(u"file_name_lineEdit")
+        self.file_name_lineEdit.setGeometry(QRect(410, 160, 113, 22))
+        self.select_existing_excel_btn = QPushButton(self.centralwidget)
+        self.select_existing_excel_btn.setObjectName(u"select_existing_excel_btn")
+        self.select_existing_excel_btn.setGeometry(QRect(400, 350, 181, 51))
+        self.write_existing_btn = QPushButton(self.centralwidget)
+        self.write_existing_btn.setObjectName(u"write_existing_btn")
+        self.write_existing_btn.setGeometry(QRect(400, 420, 181, 41))
+        self.selected_xlsx_lbl = QLabel(self.centralwidget)
+        self.selected_xlsx_lbl.setObjectName(u"selected_xlsx_lbl")
+        self.selected_xlsx_lbl.setGeometry(QRect(600, 340, 181, 41))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.select_txt_btn.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u0442\u044c \u0444\u0430\u0439\u043b", None))
+        self.selected_txt_lbl.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u043d \u0444\u0430\u0439\u043b", None))
+        self.write_new_btn.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043f\u0438\u0441\u0430\u0442\u044c \u0432 \u043d\u043e\u0432\u044b\u0439", None))
+        self.file_name_lineEdit.setText(QCoreApplication.translate("MainWindow", u"logs <timestamp>", None))
+        self.select_existing_excel_btn.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u0442\u044c \u0441\u0443\u0449\u0435\u0441\u0442\u0432\u0443\u044e\u0449\u0438\u0439", None))
+        self.write_existing_btn.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043f\u0438\u0441\u0430\u0442\u044c \u0432 \u0441\u0443\u0449\u0435\u0441\u0442\u0443\u044e\u0449\u0438\u0439", None))
+        self.selected_xlsx_lbl.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u043d \u0444\u0430\u0439\u043b", None))
+    # retranslateUi
+
